ae2580f3a848dc6a053d31141ecdf4b586765a0c
philipchi <cyuhsuan@sdsc.edu>
2024-05-02 16:24:45 -0700

    add unit tests

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Lab 5 - Starter
-Philip Chi and Christopher Schrader
+Philip Chi and Christopher Schrader  
+
+1. I would not use a unit test to test the "message" feature of a messaging application because the feature is a combination of multiple components in both frontend and backend. Unit tests are designed to test individual components of a program, therefore, the "message" feature as a whole is not suitable for unit tests.
+2. I would use a unit test to test the “max message length” feature of a messaging application because it is a component of the "message" feature that can be easily tested with a few tests because it does not involve many moving components.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,67 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+test('test isPhoneNumber to be false', () => {
+  expect(isPhoneNumber('')).toBe(false);
+});
+test('test isPhoneNumber to be false', () => {
+  expect(isPhoneNumber('902')).toBe(false);
+});
+test('test isPhoneNumber to be true', () => {
+  expect(isPhoneNumber('000-000-0000')).toBe(true);
+});
+test('test isPhoneNumber to be true', () => {
+  expect(isPhoneNumber('831-343-1574')).toBe(true);
+});
+
+test('test isEmail to be false', () => {
+  expect(isEmail('')).toBe(false);
+});
+test('test isEmail to be false', () => {
+  expect(isEmail('jonathanJoestar')).toBe(false);
+});
+test('test isEmail to be true', () => {
+  expect(isEmail('y2chi@ucsd.edu')).toBe(true);
+});
+test('test isEmail to be true', () => {
+  expect(isEmail('tow@gmail.com')).toBe(true);
+});
+
+test('test isStrongPassword to be false', () => {
+  expect(isStrongPassword('')).toBe(false);
+});
+test('test isStrongPassword to be false', () => {
+  expect(isStrongPassword('ga')).toBe(false);
+});
+test('test isStrongPassword to be true', () => {
+  expect(isStrongPassword('StrongPi23__')).toBe(true);
+});
+test('test isStrongPassword to be true', () => {
+  expect(isStrongPassword('Fien32_')).toBe(true);
+});
+
+test('test isDate to be false', () => {
+  expect(isDate('')).toBe(false);
+});
+test('test isDate to be false', () => {
+  expect(isDate('00/00/1')).toBe(false);
+});
+test('test isDate to be true', () => {
+  expect(isDate('00/00/0000')).toBe(true);
+});
+test('test isDate to be true', () => {
+  expect(isDate('12/1/2324')).toBe(true);
+});
+
+test('test isHexColor to be false', () => {
+  expect(isHexColor('')).toBe(false);
+});
+test('test isHexColor to be false', () => {
+  expect(isHexColor('zzz')).toBe(false);
+});
+test('test isHexColor to be true', () => {
+  expect(isHexColor('ffffff')).toBe(true);
+});
+test('test isHexColor to be true', () => {
+  expect(isHexColor('002342')).toBe(true);
+});
